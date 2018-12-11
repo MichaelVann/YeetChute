2208a335d84c0ae0cc73f665ade75417a1834ea2
Michael Vann <michaelwvann@gmail.com>
2018-12-11 23:54:24 +1000

    Added the main unity project; omitted most of the meta data, some remains but may be important (probs not but being cautious)

---
+++ Assets/BallHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallHandler : MonoBehaviour {
+
+    public GameObject m_ballRef;
+
+    GameObject[] m_balls;
+    float m_tunnelRadius = 12f;
+    public int m_ballCount = 42;
+
+	// Use this for initialization
+	void Start () {
+        m_balls = new GameObject[m_ballCount];
+        for (int i = 0; i < m_ballCount; i++)
+        {
+            m_balls[i] = Instantiate<GameObject>(m_ballRef, transform);
+            float angle = i * 2*Mathf.PI / (m_ballCount);
+            m_balls[i].transform.position = new Vector3(Mathf.Cos(angle) * m_tunnelRadius, Mathf.Sin(angle) * m_tunnelRadius, 0);// - new Vector3(-6,-6,0);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Cylinder/CylinderSegmentHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderSegmentHandler : MonoBehaviour {
+
+    public float m_rotateSpeed = 0.5f;
+
+    public GameObject m_barrierRef;
+    GameObject[] m_barriers;
+    int m_sideCapacity = 32;
+
+    void GenerateBarriers()
+    {
+        List<int> barrierSlots = new List<int>(m_sideCapacity);//Create a list containing the potential random positions
+        for (int i = 0; i < m_sideCapacity; i++)
+        {
+            barrierSlots.Add(i);
+        }
+
+        m_barriers = new GameObject[m_sideCapacity];//Instantiate Barrier list
+
+        for (int i = 0; i < m_sideCapacity;)//For all the barrier slots
+        {
+            m_barriers[i] = Instantiate<GameObject>(m_barrierRef, transform);//Instantiate the barrier 
+
+            float segmentRotation = (i + 1) * (360 / m_sideCapacity);//Rotate it to the appropriate position
+            m_barriers[i].transform.rotation = Quaternion.Euler(new Vector3(0, 0, segmentRotation));//..
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        GenerateBarriers();
+    }
+
+    void HandleInput()
+    {
+        float deltaRot = 0;
+        if (Input.GetKey(KeyCode.A))
+            deltaRot -= m_rotateSpeed;
+
+        if (Input.GetKey(KeyCode.D))
+            deltaRot += m_rotateSpeed;
+
+        transform.Rotate(0, 0, deltaRot);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        HandleInput();
+    }
+
+}
